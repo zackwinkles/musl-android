b45cc34cddf5942beeb809d1802064876d18e625
Zack Winkles <zmw@disroot.org>
2024-03-29 20:10:12 -0700

    Updated version name to include -android suffix.  Drop -git suffix in favor of this.

--- tools/version.sh
+++ tools/version.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 if test -d .git ; then
-if type git >/dev/null 2>&1 ; then
-git describe --tags --match 'v[0-9]*' 2>/dev/null \
-| sed -e 's/^v//' -e 's/-/-git-/'
-else
-sed 's/$/-git/' < VERSION
+	if type git >/dev/null 2>&1 ; then
+		git describe --tags --match 'v[0-9]*' 2>/dev/null
+		exit 0
+	fi
 fi
-else
+
 cat VERSION
-fi
+
