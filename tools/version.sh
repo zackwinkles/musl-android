f7a3528f23ae7466effa11ea5255fa5d9442b19c
Zack Winkles <zmw@disroot.org>
2024-04-04 09:31:19 -0700

    Revision of tools/version.sh.  Hopefully the VERSION mods ensure the -android- part stays in NO MATTER WHAT.  I don't want poor Rich (lol, Poor Richard's Almanac anybody?) to get blamed for my mistakes.  +1 Rich Felker

--- tools/version.sh
+++ tools/version.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 if test -d .git ; then
-	if type git >/dev/null 2>&1 ; then
-		git describe --tags --match 'v[0-9]*' 2>/dev/null
-		exit 0
-	fi
+if type git >/dev/null 2>&1 ; then
+git describe --tags --match 'v[0-9]*' 2>/dev/null \
+| sed -e 's/^v//' -e 's/-/-android-/'
+else
+sed 's/$/-android/' < VERSION
 fi
-
+else
 cat VERSION
-
+fi
