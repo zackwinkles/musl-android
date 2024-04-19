b49e05083dcd0810c2aed5c9617d3b6cf5b437d4
Zack Winkles <zmw@disroot.org>
2024-04-19 04:27:45 -0700

    Added optional extra precision to constants in include/math.h.

--- include/math.h
+++ include/math.h
@@ -366,7 +366,21 @@ long double truncl(long double);
 #define MAXFLOAT        3.40282346638528859812e+38F
 #endif
 
-#if defined(_XOPEN_SOURCE) || defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
+#if defined(_GNU_SOURCE)
+#define M_E				2.718281828459045235360287471352662498L /* e */
+#define M_LOG2E			1.442695040888963407359924681001892137L /* log 2e */
+#define M_LOG10E		0.434294481903251827651128918916605082L /* log 10e */
+#define M_LN2			0.693147180559945309417232121458176568L /* log e2 */
+#define M_LN10			2.302585092994045684017991454684364208L /* log e10 */
+#define M_PI			3.141592653589793238462643383279502884L /* pi */
+#define M_PI_2			1.570796326794896619231321691639751442L /* pi/2 */
+#define M_PI_4			0.785398163397448309615660845819875721L /* pi/4 */
+#define M_1_PI			0.318309886183790671537767526745028724L /* 1/pi */
+#define M_2_PI			0.636619772367581343075535053490057448L /* 2/pi */
+#define M_2_SQRTPI		1.128379167095512573896158903121545172L /* 2/sqrt(pi) */
+#define M_SQRT2			1.414213562373095048801688724209698079L /* sqrt(2) */
+#define M_SQRT1_2		0.707106781186547524400844362104849039L /* 1/sqrt(2) */
+#elif defined(_XOPEN_SOURCE) || defined(_BSD_SOURCE)
 #define M_E             2.7182818284590452354   /* e */
 #define M_LOG2E         1.4426950408889634074   /* log_2 e */
 #define M_LOG10E        0.43429448190325182765  /* log_10 e */
@@ -380,7 +394,9 @@ long double truncl(long double);
 #define M_2_SQRTPI      1.12837916709551257390  /* 2/sqrt(pi) */
 #define M_SQRT2         1.41421356237309504880  /* sqrt(2) */
 #define M_SQRT1_2       0.70710678118654752440  /* 1/sqrt(2) */
+#endif
 
+#if defined(_XOPEN_SOURCE) || defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
 extern int signgam;
 
 double      j0(double);
